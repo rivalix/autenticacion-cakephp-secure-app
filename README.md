d51679efdeb0215d6f98e8bd37b244e715df7046
Raul Alvarez <raul.alva1926@gmail.com>
2022-02-02 14:43:14 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Autenticacion y Autorizacion en CakePHP 4
+
+
+[CakePHP](https://cakephp.org) 4.x.
+
+
+## Instalacion
+
+1. Clonar repositorio
+2. Configurar conexion a base de datos en **config/app_local.php**
+
+```bash
+'username' => 'root',
+'password' => '',
+'database' => 'secure-app',
+```
+
+3. Ejecutar comando **bin/cake server**
+
+Autor: Raul P. Alvarez - [YouTube - WannaCode](https://www.youtube.com/c/WannaCode)
---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'email' => 'Lorem ipsum dolor sit amet',
+                'phone_number' => 'Lorem ipsum dolor sit amet',
+                'country_code' => 'Lorem ipsum dolor sit amet',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'created' => '2022-02-02 20:13:45',
+                'modified' => '2022-02-02 20:13:45',
+            ],
+        ];
+        parent::init();
+    }
+}
